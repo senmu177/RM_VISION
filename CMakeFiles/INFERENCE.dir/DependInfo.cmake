171e4aad523553da36d696e9eeece1af310527d5
ZipperWang <528771345@qq.com>
2025-09-27 16:58:06 +0800

    init

---
+++ CMakeFiles/INFERENCE.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adminpc/ViSION/Reborn-Vision-2024-armor-Inference-main/src/inference.cpp" "CMakeFiles/INFERENCE.dir/src/inference.cpp.o" "gcc" "CMakeFiles/INFERENCE.dir/src/inference.cpp.o.d"
+  "/home/adminpc/ViSION/Reborn-Vision-2024-armor-Inference-main/src/number_classifier.cpp" "CMakeFiles/INFERENCE.dir/src/number_classifier.cpp.o" "gcc" "CMakeFiles/INFERENCE.dir/src/number_classifier.cpp.o.d"
+  "/home/adminpc/ViSION/Reborn-Vision-2024-armor-Inference-main/src/tradition.cpp" "CMakeFiles/INFERENCE.dir/src/tradition.cpp.o" "gcc" "CMakeFiles/INFERENCE.dir/src/tradition.cpp.o.d"
+  "/home/adminpc/ViSION/Reborn-Vision-2024-armor-Inference-main/src/video_detect_test.cpp" "CMakeFiles/INFERENCE.dir/src/video_detect_test.cpp.o" "gcc" "CMakeFiles/INFERENCE.dir/src/video_detect_test.cpp.o.d"
+  "" "INFERENCE" "gcc" "CMakeFiles/INFERENCE.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
